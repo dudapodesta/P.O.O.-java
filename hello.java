3ed6816f08a87d9f448c09fc97450ce516463626
dudapodesta <48567515+dudapodesta@users.noreply.github.com>
2024-04-26 09:59:37 -0300

    Update and rename digitos.java to hello.java

---
+++ hello.java
@@ -0,0 +1,12 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Ola!\n");
+        System.out.println("Boa tarde");
+        System.out.println("Hello, World!"); 
+    }
+}  
+
+
+
+//Para compilar: java hello.java
