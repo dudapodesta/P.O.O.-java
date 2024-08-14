dc18be86bb1a78d4f49391bd7ccfa978ecf594fe
dudapodesta <48567515+dudapodesta@users.noreply.github.com>
2024-08-14 13:47:11 -0300

    Add files via upload

---
+++ imprime.java
@@ -0,0 +1,9 @@
+//imprimindo texto e variaveis
+
+class Imprime {
+    public static void main(String[] args) {
+	int x=10;
+        System.out.println("O valor de x é " + x); 
+	//System.out.println(x);
+    }
+}  
