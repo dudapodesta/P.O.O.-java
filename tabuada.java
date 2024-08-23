34a2f5f3103bc5367caf1eb27d8f730921797994
dudapodesta <48567515+dudapodesta@users.noreply.github.com>
2024-08-23 14:05:44 -0300

    Add files via upload

---
+++ tabuada.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Tabuada {
+
+  public static void main(String[] args) {
+    int i,j,p;
+    for(i=1;i<=10;i++)
+        {
+            for(j=1;j<=10;j++)
+            {
+                p=i*j;
+                System.out.println(i+"X"+j+"="+p);
+            }
+            System.out.print("\n");
+        }
+  }
+}
+
+
+
