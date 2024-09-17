8dec9e7b2ba7d399bbedc5f04f1ab5396ff5ade1
dudapodesta <48567515+dudapodesta@users.noreply.github.com>
2024-09-17 15:46:53 -0300

    Add files via upload

---
+++ fila-naive.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+import java.io.IOException;
+public class Fila {
+
+  public static void main(String[] args) throws IOException {
+    int num,fim,inicio,i;
+    int[] vec = new int[100];
+    char op;
+    boolean flag;
+    //abre o scanner
+    Scanner scan = new Scanner(System.in);
+    flag=true;
+    inicio = 0;
+    fim = 0;
+    while(flag){
+       op = (char)System.in.read();
+       switch(op){ 
+          //empilha
+          case 'e':
+             vec[fim] = scan.nextInt();
+             fim++;
+             break;
+          //desempilha
+          case 'd':
+             if(fim==inicio){ 
+                System.out.println("Fila vazia");
+             }
+             else{   
+                System.out.println("Sai da fila: "+vec[inicio]);
+                inicio++;
+             }
+             break;
+          //imprime:
+          case 'i':
+             if(fim==inicio){ 
+                System.out.println("Fila vazia");
+             }
+             else{  
+                System.out.println("Fila:"); 
+                for(i=inicio;i<fim;i++){
+                   System.out.print(vec[i]+" ");
+                }
+             }
+             
+             break;
+          //fim
+          case 'f':
+              flag=false;
+              break;
+          
+          //espaço 'extra
+          case '\n':
+              break;
+          //erro
+          default:
+              System.out.println("Comando inválido ");
+       }
+    }
+    // fecha scanner
+    scan.close();
+    
+    }
+}
+
+
---
+++ pilha-naive.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+import java.io.IOException;
+public class Pilha {
+
+  public static void main(String[] args) throws IOException {
+    int num,topo,fundo,i;
+    int[] vec = new int[100];
+    char op;
+    boolean flag;
+    //abre o scanner
+    Scanner scan = new Scanner(System.in);
+    flag=true;
+    topo = 0;
+    fundo = 0;
+    while(flag){
+       op = (char)System.in.read();
+       switch(op){ 
+          //empilha
+          case 'e':
+             vec[topo] = scan.nextInt();
+             topo++;
+             break;
+          //desempilha
+          case 'd':
+             if(topo==fundo){ 
+                System.out.println("Pilha vazia");
+             }
+             else{   
+                System.out.println("Elemento desempilhado: "+vec[topo-1]);
+                topo--;
+             }
+             break;
+          //imprime:
+          case 'i':
+             if(topo==fundo){ 
+                System.out.println("Pilha vazia");
+             }
+             else{  
+                System.out.println("Pilha:"); 
+                for(i=topo-1;i>=fundo;i--){
+                   System.out.println(vec[i]);
+                }
+             }
+             
+             break;
+          //fim
+          case 'f':
+              flag=false;
+              break;
+          
+          //espaço 'extra
+          case '\n':
+              break;
+          //erro
+          default:
+              System.out.println("Comando inválido ");
+       }
+    }
+    // fecha scanner
+    scan.close();
+    
+    }
+}
+
+
