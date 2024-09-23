eda64536a462785c43260dc0bdbad6b359337189
dudapodesta <48567515+dudapodesta@users.noreply.github.com>
2024-09-23 13:33:18 -0300

    Add files via upload

---
+++ detmat.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class LerMat {
+
+  public static void main(String[] args) {
+    int i,j,det;
+    int[][] a = new int[3][3];
+    //abre o scanner
+    Scanner scan = new Scanner(System.in);
+    System.out.print("Entre com uma matriz de inteiros de tamanho 3x3: ");
+    //i linhas
+    for(i=0;i<3;i++)
+    {
+        //j colunas
+        for(j=0;j<3;j++){
+           a[i][j] = scan.nextInt();    
+       }
+    }
+    // fecha scanner
+    scan.close();
+    //calcula o determinante
+    det = a[0][0]*a[1][1]*a[2][2]+a[1][0]*a[2][1]*a[0][2]+a[2][0]*a[0][1]*a[1][2];
+    det -= (a[0][2]*a[1][1]*a[2][0]+a[1][0]*a[2][2]*a[0][1]+a[0][0]*a[2][1]*a[1][2]);
+    System.out.println("O determinante é: "+det);
+  }
+  
+}
