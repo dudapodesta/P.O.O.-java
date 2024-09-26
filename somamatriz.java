bb5a11d50f7859fe33cb0f7c2f94ad96d403840c
dudapodesta <48567515+dudapodesta@users.noreply.github.com>
2024-09-26 13:06:39 -0300

    Update somamatriz.java

--- somamatriz.java
+++ somamatriz.java
@@ -3,42 +3,43 @@ public class SomaMat {
 
   public static void main(String[] args) {
     int i,j;
-    int[][] mat1 = new int[2][4];
-    int[][] mat2 = new int[2][4];
+    int[][] mat1 = new int[3][3];
+    int[][] mat2 = new int[3][3];
     //abre o scanner
     Scanner scan = new Scanner(System.in);
-    System.out.print("Entre com uma matriz de inteiros de tamanho 2x4: ");
+    System.out.print("Entre com uma matriz de inteiros de tamanho 3x3: ");
     //i linhas
-    for(i=0;i<2;i++)
+    for(i=0;i<3;i++)
     {
         //j colunas
-        for(j=0;j<4;j++){
-           mat1[i][j] = scan.nextInt();    
+        for(j=0;j<3;j++){
+          //insira aqui o código para somar as duas matrizes termo a termo
+          
        }
     }
-    System.out.print("Entre com uma matriz de inteiros de tamanho 2x4: ");
+    System.out.print("Entre com uma matriz de inteiros de tamanho 3x3: ");
     //i linhas
-    for(i=0;i<2;i++)
+    for(i=0;i<3;i++)
     {
         //j colunas
-        for(j=0;j<4;j++){
+        for(j=0;j<3;j++){
            mat2[i][j] = scan.nextInt();    
        }
     }
     // fecha scanner
     scan.close();
     // soma as matrizes e guarda em mat1
-    for(i=0;i<2;i++)
+    for(i=0;i<3;i++)
     {
-        for(j=0;j<4;j++){
+        for(j=0;j<3;j++){
            mat1[i][j]+=mat2[i][j];    
        }
     }
     
     // imprime a matriz
-    for(i=0;i<2;i++)
+    for(i=0;i<3;i++)
     {
-        for(j=0;j<4;j++){
+        for(j=0;j<3;j++){
            System.out.print(mat1[i][j]+" ");    
        }
        System.out.print("\n");
