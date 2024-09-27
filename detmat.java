11bd9670e21b31b17611b195a6a66d4b825239e3
dudapodesta <48567515+dudapodesta@users.noreply.github.com>
2024-09-27 13:39:19 -0300

    Update detmat.java

--- detmat.java
+++ detmat.java
@@ -18,7 +18,8 @@ public class DetMat {
     // fecha scanner
     scan.close();
     //insira aqui o cálculo do determinante
-    
+    det = a[0][0]*a[1][1]*a[2][2]+a[1][0]*a[2][1]*a[0][2]+a[2][0]*a[0][1]*a[1][2];
+    det = det - (a[0][2]*a[1][1]*a[2][0]+a[1][2]*a[2][1]*a[0][0]+a[2][2]*a[0][1]*a[1][0]);
     
     System.out.println("O determinante é: "+det);
   }
